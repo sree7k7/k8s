889e506e594221ce1958fc787585f1aa36672b84
sree7k7 <sree7k7@gmail.com>
2023-06-14 12:57:27 +0000

    test

--- service/pod-service-example/gitpull.sh
+++ service/pod-service-example/gitpull.sh
@@ -2,4 +2,12 @@
 
 #git pull
 
-while true; do git pull; sleep 5; done
+while true; 
+do 
+	git pull; 
+	sleep 5; 
+	git add .
+        git commit -m "test"
+        git push
+        sleep 2;
+done
